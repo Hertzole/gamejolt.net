8ff7f2f121200425e8b06883769370ea58de84df
Hertzole <hampusringstrom@gmail.com>
2024-01-07 05:52:08 +0100

    refactor: move poolhandle to it's own file

--- GameJolt.NET/Pooling/ObjectPool.cs
+++ GameJolt.NET/Pooling/ObjectPool.cs
@@ -41,21 +41,4 @@ namespace Hertzole.GameJolt
 			pool.Push(obj);
 		}
 	}
-
-	internal readonly struct PoolHandle<T> : IDisposable where T : class
-	{
-		private readonly ObjectPool<T> pool;
-		private readonly T obj;
-
-		public PoolHandle(ObjectPool<T> pool, T obj)
-		{
-			this.pool = pool;
-			this.obj = obj;
-		}
-
-		public void Dispose()
-		{
-			pool.Return(obj);
-		}
-	}
 }
---
+++ GameJolt.NET/Pooling/PoolHandle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hertzole.GameJolt
+{
+	internal readonly struct PoolHandle<T> : IDisposable where T : class
+	{
+		private readonly ObjectPool<T> pool;
+		private readonly T obj;
+
+		public PoolHandle(ObjectPool<T> pool, T obj)
+		{
+			this.pool = pool;
+			this.obj = obj;
+		}
+
+		public void Dispose()
+		{
+			pool.Return(obj);
+		}
+	}
+}
