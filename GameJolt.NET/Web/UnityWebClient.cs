2ca142fb0a999f6d8b8ba72fec377196576d55ec
Hertzole <hampusringstrom@gmail.com>
2024-02-04 12:53:34 +0100

    perf: use Unity's Awaitable in 2023.1+ when calling the web

--- GameJolt.NET/Web/UnityWebClient.cs
+++ GameJolt.NET/Web/UnityWebClient.cs
@@ -1,18 +1,33 @@
 ﻿#if UNITY_2021_1_OR_NEWER
+
+using System.Threading;
+using UnityEngine.Networking;
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER || UNITY_2021_3_OR_NEWER
 using StringTask = System.Threading.Tasks.ValueTask<string>;
 #else
 using StringTask = System.Threading.Tasks.Task<string>;
 #endif
-using System.Threading;
+#if !UNITY_2023_1_OR_NEWER
 using System.Threading.Tasks;
-using UnityEngine.Networking;
+#else
+using UnityEngine;
+#endif
 
 namespace Hertzole.GameJolt
 {
 	internal sealed class UnityWebClient : IGameJoltWebClient
 	{
 		public async StringTask GetStringAsync(string url, CancellationToken cancellationToken)
+		{
+#if UNITY_2023_1_OR_NEWER
+			return await GetStringAsyncAwaitable(url, cancellationToken);
+#else
+			return await GetStringAsyncCompletionSource(url, cancellationToken).ConfigureAwait(false);
+#endif
+		}
+
+#if !UNITY_2023_1_OR_NEWER
+		private static async StringTask GetStringAsyncCompletionSource(string url, CancellationToken cancellationToken)
 		{
 			TaskCompletionSource<string> tcs = new TaskCompletionSource<string>();
 
@@ -45,6 +60,23 @@ namespace Hertzole.GameJolt
 
 			return await tcs.Task.ConfigureAwait(false);
 		}
+#else
+		private static async Awaitable<string> GetStringAsyncAwaitable(string url, CancellationToken cancellationToken)
+		{
+			UnityWebRequest request = UnityWebRequest.Get(url);
+
+			await request.SendWebRequest();
+			
+			cancellationToken.ThrowIfCancellationRequested();
+			
+			if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+			{
+				throw new GameJoltException(request.error);
+			}
+			
+			return request.downloadHandler.text;
+		}
+#endif
 
 		public void Dispose()
 		{
