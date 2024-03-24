095a1f46e961cb8dbcd6fde2b8c2fd3b666f8a6e
Hertzole <hampusringstrom@gmail.com>
2024-03-24 03:40:26 +0100

    feat: OnInitialized, OnShutdown, and OnShutdownComplete events
    This will help in the future.

--- GameJolt.NET.Tests/ApiTest.cs
+++ GameJolt.NET.Tests/ApiTest.cs
@@ -46,5 +46,51 @@ namespace GameJolt.NET.Tests
 		{
 			Assert.Throws<GameJoltInitializationException>(() => { _ = GameJoltAPI.Friends; });
 		}
+
+		[Test]
+		public void Initialize_OnInitialized_IsInvoked()
+		{
+			bool invoked = false;
+			GameJoltAPI.OnInitialized += () => invoked = true;
+
+			GameJoltAPI.Initialize(0, "");
+
+			Assert.That(invoked, Is.True);
+			
+			GameJoltAPI.Shutdown(); // Clean up.
+		}
+
+		[Test]
+		public void Shutdown_OnShutdown_IsInvoked()
+		{
+			bool invoked = false;
+			GameJoltAPI.OnShutdown += () =>
+			{
+				invoked = true;
+				// It should still be initialized until the shutdown is complete.
+				Assert.That(GameJoltAPI.IsInitialized, Is.True);
+			};
+
+			GameJoltAPI.Initialize(0, "");
+			GameJoltAPI.Shutdown();
+
+			Assert.That(invoked, Is.True);
+		}
+		
+		[Test]
+		public void ShutdownComplete_OnShutdownComplete_IsInvoked()
+		{
+			bool invoked = false;
+			GameJoltAPI.OnShutdownComplete += () =>
+			{
+				invoked = true;
+				Assert.That(GameJoltAPI.IsInitialized, Is.False);
+			};
+
+			GameJoltAPI.Initialize(0, "");
+			GameJoltAPI.Shutdown();
+
+			Assert.That(invoked, Is.True);
+		}
 	}
 }
