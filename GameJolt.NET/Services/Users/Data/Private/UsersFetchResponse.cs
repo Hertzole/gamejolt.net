07b6a6ae8c590b9aecd15e1bdcc2b7392b3f82df
Hertzole <hampusringstrom@gmail.com>
2024-04-07 20:33:51 +0200

    chore: add IEquatable to all user types

--- GameJolt.NET/Services/Users/Data/Private/UsersFetchResponse.cs
+++ GameJolt.NET/Services/Users/Data/Private/UsersFetchResponse.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 
+using System;
 #if NET6_0_OR_GREATER
 using JsonName = System.Text.Json.Serialization.JsonPropertyNameAttribute;
 using JsonConverter = System.Text.Json.Serialization.JsonConverterAttribute;
@@ -13,7 +14,7 @@ using JsonConstructor = Newtonsoft.Json.JsonConstructorAttribute;
 
 namespace Hertzole.GameJolt
 {
-	internal readonly struct UsersFetchResponse : IResponse
+	internal readonly struct UsersFetchResponse : IResponse, IEquatable<UsersFetchResponse>
 	{
 		[JsonName("success")]
 		[JsonConverter(typeof(GameJoltBooleanConverter))]
@@ -37,6 +38,36 @@ namespace Hertzole.GameJolt
 			Success = success;
 			Message = message;
 			Users = new[] { user };
+		}
+
+		public bool Equals(UsersFetchResponse other)
+		{
+			return EqualityHelper.ResponseEquals(this, other) && EqualityHelper.ArrayEquals(Users, other.Users);
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return obj is UsersFetchResponse other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hashCode = EqualityHelper.ResponseHashCode(0, this);
+				hashCode = (hashCode * 397) ^ Users.GetHashCode();
+				return hashCode;
+			}
+		}
+
+		public static bool operator ==(UsersFetchResponse left, UsersFetchResponse right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(UsersFetchResponse left, UsersFetchResponse right)
+		{
+			return !left.Equals(right);
 		}
 	}
 }
