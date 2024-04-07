ae97c979806fb957bb09770b1008fab0763bceba
Hertzole <hampusringstrom@gmail.com>
2024-04-07 20:44:11 +0200

    chore: add IEquatable to GameJoltResponse

--- GameJolt.NET/Core/GameJoltResponse.cs
+++ GameJolt.NET/Core/GameJoltResponse.cs
@@ -1,10 +1,12 @@
-﻿#if NET6_0_OR_GREATER
+﻿using System;
+using System.Collections.Generic;
+#if NET6_0_OR_GREATER
 using System.Text.Json.Serialization;
 #endif
 
 namespace Hertzole.GameJolt
 {
-	internal readonly struct GameJoltResponse<T>
+	internal readonly struct GameJoltResponse<T> : IEquatable<GameJoltResponse<T>>
 	{
 		public readonly T response;
 
@@ -14,6 +16,31 @@ namespace Hertzole.GameJolt
 		public GameJoltResponse(T response)
 		{
 			this.response = response;
+		}
+
+		public bool Equals(GameJoltResponse<T> other)
+		{
+			return EqualityComparer<T>.Default.Equals(response, other.response);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is GameJoltResponse<T> other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return EqualityComparer<T>.Default.GetHashCode(response);
+		}
+
+		public static bool operator ==(GameJoltResponse<T> left, GameJoltResponse<T> right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(GameJoltResponse<T> left, GameJoltResponse<T> right)
+		{
+			return !left.Equals(right);
 		}
 	}
 }
