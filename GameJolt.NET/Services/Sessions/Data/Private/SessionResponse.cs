06a70b5a78811b1313324e59f94e18b07b97fc0e
Hertzole <hampusringstrom@gmail.com>
2024-04-07 20:02:37 +0200

    chore: add IEquatable to all session types

--- GameJolt.NET/Services/Sessions/Data/Private/SessionResponse.cs
+++ GameJolt.NET/Services/Sessions/Data/Private/SessionResponse.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 
+using System;
 #if NET6_0_OR_GREATER
 using JsonName = System.Text.Json.Serialization.JsonPropertyNameAttribute;
 using JsonConverter = System.Text.Json.Serialization.JsonConverterAttribute;
@@ -13,7 +14,7 @@ using JsonConstructor = Newtonsoft.Json.JsonConstructorAttribute;
 
 namespace Hertzole.GameJolt
 {
-	internal readonly struct SessionResponse : IResponse
+	internal readonly struct SessionResponse : IResponse, IEquatable<SessionResponse>
 	{
 		[JsonName("success")]
 		[JsonConverter(typeof(GameJoltBooleanConverter))]
@@ -26,6 +27,31 @@ namespace Hertzole.GameJolt
 		{
 			Success = success;
 			Message = message;
+		}
+
+		public bool Equals(SessionResponse other)
+		{
+			return EqualityHelper.ResponseEquals(this, other);
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return obj is SessionResponse other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return EqualityHelper.ResponseHashCode(0, this);
+		}
+
+		public static bool operator ==(SessionResponse left, SessionResponse right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(SessionResponse left, SessionResponse right)
+		{
+			return !left.Equals(right);
 		}
 	}
 }
