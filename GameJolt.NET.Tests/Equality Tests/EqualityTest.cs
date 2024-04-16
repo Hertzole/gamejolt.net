06dde03eca186a9a61e4e20199128f72d5a5b722
Hertzole <hampusringstrom@gmail.com>
2024-04-16 19:40:18 +0200

    test: fix equality tests not using the proper Equals method

--- GameJolt.NET.Tests/Equality Tests/EqualityTest.cs
+++ GameJolt.NET.Tests/Equality Tests/EqualityTest.cs
@@ -13,7 +13,7 @@ namespace GameJolt.NET.Tests.Equality
 			object bObj = b;
 
 			Assert.That(a, Is.EqualTo(b), "First value should be equal to itself.");
-			Assert.That(a, Is.EqualTo(bObj), "First value should be equal to itself in object form.");
+			Assert.That(a.Equals(bObj), Is.True, "First value should be equal to itself in object form.");
 			Assert.That(a.GetHashCode(), Is.EqualTo(b.GetHashCode()), "First value should have the same hash code as itself.");
 			Assert.That(compare.Invoke(a, b), Is.True, "First value should be equal to itself by comparison.");
 
@@ -23,7 +23,7 @@ namespace GameJolt.NET.Tests.Equality
 				bObj = b;
 
 				Assert.That(a, Is.Not.EqualTo(b), "First value should not be equal to the second value.");
-				Assert.That(a, Is.Not.EqualTo(bObj), "First value should not be equal to the second value in object form.");
+				Assert.That(a.Equals(bObj), Is.False, "First value should not be equal to the second value in object form.");
 				Assert.That(a.GetHashCode(), Is.Not.EqualTo(b.GetHashCode()), "First value should not have the same hash code as the second value.");
 				Assert.That(invertCompare.Invoke(a, b), Is.True, "First value should not be equal to the second value by comparison.");
 			}
