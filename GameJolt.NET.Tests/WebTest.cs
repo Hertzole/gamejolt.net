dd97b63452be1f61a44f93b39eedd65814bb7576
Hertzole <contact@hertzole.se>
2024-05-05 20:28:54 +0200

    test: web client tests
    Now includes tests for failing and canceled tasks.

--- GameJolt.NET.Tests/WebTest.cs
+++ GameJolt.NET.Tests/WebTest.cs
@@ -1,19 +1,80 @@
 ﻿#nullable enable
 
+#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER || UNITY_2021_3_OR_NEWER
+using StringTask = System.Threading.Tasks.ValueTask<string>;
+#else
+using StringTask = System.Threading.Tasks.Task<string>;
+#endif
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Hertzole.GameJolt;
 using NUnit.Framework;
 
 namespace GameJolt.NET.Tests
 {
-	public class WebTest
+	public sealed class WebTest
 	{
+		private readonly IGameJoltWebClient webClient = GameJoltAPI.GetWebClient();
+
 		[Test]
-		public async Task SendRequest()
+		public async Task SendRequest_Success()
 		{
-			string? str = await GameJoltAPI.webClient.GetStringAsync("https://api.gamejolt.com/api/game/v1_2/time", default);
-			
+			string? str = await webClient.GetStringAsync("https://httpbin.org/get", default);
+
 			Assert.That(str, Is.Not.Null);
+		}
+
+		[Test]
+		[TestCase(404)]
+		[TestCase(500)]
+		public async Task SendRequest_Fail(int errorCode)
+		{
+			bool caught = false;
+
+			try
+			{
+				await webClient.GetStringAsync($"https://httpbin.org/status/{errorCode}", default);
+			}
+			catch (HttpRequestException)
+			{
+				caught = true;
+			}
+
+			// Can't really use Assert.ThrowsAsync here since it freezes Unity. So we have to do it manually. ¯\_(ツ)_/¯
+			Assert.That(caught, Is.True);
+		}
+
+		[Test]
+		public async Task SendRequest_Cancel([Values] bool beforeRequest)
+		{
+			CancellationTokenSource cancelSource = new CancellationTokenSource();
+
+			if (beforeRequest)
+			{
+				cancelSource.Cancel();
+			}
+
+			bool caught = false;
+
+			StringTask task = webClient.GetStringAsync("https://httpbin.org/get", cancelSource.Token);
+
+			if (!beforeRequest)
+			{
+				cancelSource.Cancel();
+			}
+
+			try
+			{
+				await task;
+			}
+			catch (TaskCanceledException)
+			{
+				caught = true;
+			}
+
+			// Can't really use Assert.ThrowsAsync here since it freezes Unity. So we have to do it manually. ¯\_(ツ)_/¯
+			Assert.That(caught, Is.True);
 		}
 	}
 }
