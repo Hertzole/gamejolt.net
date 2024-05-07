75cb9ec048ab488d651c45d8ccf8438f84f4dd2e
Hertzole <contact@hertzole.se>
2024-05-07 06:54:29 +0200

    refactor(test): split data store tests into partial classes
    It was getting too big.

---
+++ GameJolt.NET.Tests/Services/Data Store/DataStoreTest.GetKeys.Global.cs
@@ -0,0 +1,104 @@
+﻿using System.Threading.Tasks;
+using Hertzole.GameJolt;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace GameJolt.NET.Tests
+{
+	partial class DataStoreTest
+	{
+		[Test]
+		[TestCase("")]
+		[TestCase("*")]
+		public async Task GetKeysGlobal_Success(string pattern)
+		{
+			GameJoltAPI.webClient.GetStringAsync("", default).ReturnsForAnyArgs(info =>
+			{
+				string? arg = info.Arg<string>();
+
+				if (string.IsNullOrEmpty(pattern))
+				{
+					Assert.That(arg, Does.StartWith(GameJoltUrlBuilder.BASE_URL + GameJoltDataStore.FETCH_KEYS_ENDPOINT));
+				}
+				else
+				{
+					Assert.That(arg, Does.StartWith(GameJoltUrlBuilder.BASE_URL + GameJoltDataStore.FETCH_KEYS_ENDPOINT + $"?pattern={pattern}"));
+				}
+
+				string json = serializer.SerializeResponse(new GetKeysResponse(true, null, new[]
+				{
+					new DataKey("key1"),
+					new DataKey("key2")
+				}));
+
+				return FromResult(json);
+			});
+
+			GameJoltResult<string[]> result = await GameJoltAPI.DataStore.GetKeysAsync(pattern);
+
+			Assert.That(result.HasError, Is.False);
+			Assert.That(result.Exception, Is.Null);
+			Assert.That(result.Value, Is.Not.Null);
+			Assert.That(result.Value!.Length, Is.EqualTo(2));
+			Assert.That(result.Value[0], Is.EqualTo("key1"));
+			Assert.That(result.Value[1], Is.EqualTo("key2"));
+		}
+
+		[Test]
+		public async Task GetKeysGlobal_Success_NoKeys()
+		{
+			GameJoltAPI.webClient.GetStringAsync("", default).ReturnsForAnyArgs(info =>
+			{
+				string? arg = info.Arg<string>();
+				Assert.That(arg, Does.StartWith(GameJoltUrlBuilder.BASE_URL + GameJoltDataStore.FETCH_KEYS_ENDPOINT));
+
+				string json = serializer.SerializeResponse(new GetKeysResponse(true, null, null));
+
+				return FromResult(json);
+			});
+
+			GameJoltResult<string[]> result = await GameJoltAPI.DataStore.GetKeysAsync();
+
+			Assert.That(result.HasError, Is.False);
+			Assert.That(result.Exception, Is.Null);
+			Assert.That(result.Value, Is.Not.Null);
+			Assert.That(result.Value, Is.Empty);
+		}
+
+		[Test]
+		public async Task GetKeysGlobal_Error_Fail()
+		{
+			await AssertErrorAsync<GetKeysResponse, string[], GameJoltException>(CreateResponse, GetResult);
+			return;
+
+			GetKeysResponse CreateResponse()
+			{
+				return new GetKeysResponse(false, GameJoltException.UnknownFatalError.Message, null);
+			}
+
+			Task<GameJoltResult<string[]>> GetResult()
+			{
+				return GameJoltAPI.DataStore.GetKeysAsync();
+			}
+		}
+		
+		[Test]
+		[TestCase("")]
+		[TestCase("*")]
+		public async Task GetKeysAsync_ValidUrl(string pattern)
+		{
+			await TestUrlAsync(() => GameJoltAPI.DataStore.GetKeysAsync(pattern),
+				url =>
+				{
+					if (string.IsNullOrEmpty(pattern))
+					{
+						Assert.That(url, Does.StartWith($"{GameJoltUrlBuilder.BASE_URL}{GameJoltDataStore.FETCH_KEYS_ENDPOINT}"));
+					}
+					else
+					{
+						Assert.That(url, Does.StartWith($"{GameJoltUrlBuilder.BASE_URL}{GameJoltDataStore.FETCH_KEYS_ENDPOINT}?pattern={pattern}"));
+					}
+				});
+		}
+	}
+}
---
+++ GameJolt.NET.Tests/Services/Data Store/DataStoreTest.cs
@@ -0,0 +1,48 @@
+﻿#nullable enable
+
+using System;
+using System.Threading.Tasks;
+using Hertzole.GameJolt;
+using NUnit.Framework;
+
+namespace GameJolt.NET.Tests
+{
+	public sealed partial class DataStoreTest : BaseTest
+	{
+		// Most of all the tests were moved to the partial classes as this file was getting too big.
+		
+		[Test]
+		public async Task UpdateString_InvalidOperation_Throws()
+		{
+			bool caught = false;
+
+			try
+			{
+				await GameJoltAPI.DataStore.UpdateAsync("key", "value", (StringOperation) 10);
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+				caught = true;
+			}
+
+			Assert.That(caught, Is.True);
+		}
+
+		[Test]
+		public async Task UpdateInt_InvalidOperation_Throws()
+		{
+			bool caught = false;
+
+			try
+			{
+				await GameJoltAPI.DataStore.UpdateAsync("key", 1, (NumericOperation) 10);
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+				caught = true;
+			}
+
+			Assert.That(caught, Is.True);
+		}
+	}
+}
