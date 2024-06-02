fa8a48266829844e4ab130e484482ce3db58ba75
Hertzole <contact@hertzole.se>
2024-06-02 17:19:06 +0200

    test: settings tests

--- GameJolt.NET/Unity Integration/GameJoltSettings.cs
+++ GameJolt.NET/Unity Integration/GameJoltSettings.cs
@@ -114,22 +114,26 @@ namespace Hertzole.GameJolt
 		}
 
 #if UNITY_EDITOR
+		internal const string AUTO_SIGN_IN_KEY = "GameJolt.NET.AutoSignIn";
+		internal const string SIGN_IN_USERNAME_KEY = "GameJolt.NET.SignInUsername";
+		internal const string SIGN_IN_TOKEN_KEY = "GameJolt.NET.SignInToken";
+
 		public static bool AutoSignIn
 		{
-			get { return EditorPrefs.GetBool("GameJolt.NET.AutoSignIn", false); }
-			set { EditorPrefs.SetBool("GameJolt.NET.AutoSignIn", value); }
+			get { return EditorPrefs.GetBool(AUTO_SIGN_IN_KEY, false); }
+			set { EditorPrefs.SetBool(AUTO_SIGN_IN_KEY, value); }
 		}
 
 		public static string SignInUsername
 		{
-			get { return EditorPrefs.GetString("GameJolt.NET.SignInUsername", string.Empty); }
-			set { EditorPrefs.SetString("GameJolt.NET.SignInUsername", value); }
+			get { return EditorPrefs.GetString(SIGN_IN_USERNAME_KEY, string.Empty); }
+			set { EditorPrefs.SetString(SIGN_IN_USERNAME_KEY, value); }
 		}
 
 		public static string SignInToken
 		{
-			get { return EditorPrefs.GetString("GameJolt.NET.SignInToken", string.Empty); }
-			set { EditorPrefs.SetString("GameJolt.NET.SignInToken", value); }
+			get { return EditorPrefs.GetString(SIGN_IN_TOKEN_KEY, string.Empty); }
+			set { EditorPrefs.SetString(SIGN_IN_TOKEN_KEY, value); }
 		}
 #endif
 
