61a3a064c172195c49bfe7e72b44b006c164b0c7
Hertzole <contact@hertzole.se>
2024-12-05 04:30:35 +0100

    what is unity even doing

--- GameJolt.NET/Unity Integration/GameJoltInitialization.cs
+++ GameJolt.NET/Unity Integration/GameJoltInitialization.cs
@@ -1,14 +1,11 @@
 ﻿#if UNITY_64 && !DISABLE_GAMEJOLT
-#if GAMEJOLT_UNITY_TESTS && UNITY_INCLUDE_TESTS
-#define HAS_TESTS
-#endif // GAMEJOLT_UNITY_TESTS && DEBUG
 using System;
 using System.Threading;
 using UnityEngine;
-#if HAS_TESTS
+#if UNITY_INCLUDE_TESTS
 using NUnit.Framework;
 using NUnit.Framework.Internal;
-#endif // HAS_TESTS
+#endif // UNITY_INCLUDE_TESTS
 
 namespace Hertzole.GameJolt
 {
@@ -19,20 +16,20 @@ namespace Hertzole.GameJolt
 
 		internal static CancellationTokenSource exitTokenSource;
 
-#if HAS_TESTS
+#if UNITY_INCLUDE_TESTS
 		internal static bool ReturnIfTestIsRunning { get; set; } = true;
 #endif
 
 		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
 		private static void Initialize()
 		{
-#if HAS_TESTS && GAMEJOLT_NET_TESTS_DONT_RUN_INITIALIZATION
+#if UNITY_INCLUDE_TESTS && GAMEJOLT_NET_TESTS_DONT_RUN_INITIALIZATION
 			// Don't run this if we're running tests as it will interfere with the tests.
 			if (ReturnIfTestIsRunning)
 			{
 				return;
 			}
-#endif // HAS_TESTS
+#endif // UNITY_INCLUDE_TESTS
 
 			exitTokenSource = new CancellationTokenSource();
 
