6c48400b8b7111513459535269d72e0d1dbcd0bd
Hertzole <contact@hertzole.se>
2026-03-18 00:23:54 +0100

    feat: add parameter validation to trophy methods

---
+++ GameJolt.NET.Tests/TrophiesTest.Validation.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hertzole.GameJolt;
+using NUnit.Framework;
+
+namespace GameJolt.NET.Tests
+{
+	internal partial class TrophiesTest
+	{
+		private static readonly Predicate<ArgumentNullException> trophyResultsPredicate = e => MustNotBeNullPredicate<IList<GameJoltTrophy>>(e, "results");
+		private static readonly Predicate<ArgumentNullException> trophyIdsPredicate = e => MustNotBeNullPredicate<IEnumerable<int>>(e, "trophyIds");
+
+		[Test]
+		public async Task GetTrophies_WithList_Guards()
+		{
+			await AssertThrowsAsync(() => GameJoltAPI.Trophies.GetTrophiesAsync(((IList<GameJoltTrophy>) null)!), trophyResultsPredicate);
+		}
+
+		[Test]
+		public async Task GetTrophies_Achieved_WithList_Guards([Values] bool achieved)
+		{
+			await AssertThrowsAsync(() => GameJoltAPI.Trophies.GetTrophiesAsync(achieved, null!), trophyResultsPredicate);
+		}
+
+		[Test]
+		public async Task GetTrophies_Ids_Guards()
+		{
+			await AssertThrowsAsync(() => GameJoltAPI.Trophies.GetTrophiesAsync(((IEnumerable<int>) null)!), trophyIdsPredicate);
+		}
+
+		[Test]
+		public async Task GetTrophies_Ids_WithList_Guards()
+		{
+			// Null ids
+			await AssertThrowsAsync(() => GameJoltAPI.Trophies.GetTrophiesAsync(null!, new List<GameJoltTrophy>()), trophyIdsPredicate);
+
+			// Null results
+			await AssertThrowsAsync(() => GameJoltAPI.Trophies.GetTrophiesAsync(new[] { 1, 2, 3 }, null!), trophyResultsPredicate);
+		}
+	}
+}
